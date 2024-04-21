8be0c16ebe53a91d993d9e670882786cdce4d4c6
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-04-21 23:06:23 +0530

    Project 3 Done

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [currencyInfo, setCurrencyInfo] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((response) => response.json())
+      .then((data) => setCurrencyInfo(data[currency]));
+  }, [currency]);
+  return currencyInfo;
+}
+
+export default useCurrencyInfo;
