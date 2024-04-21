16cc159030ab7c86d204e4a6fb45eb1c027e85ef
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-04-21 14:54:03 +0530

    Project 1, 2 Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import RandomBgColor from "./components/RandomBgColor";
+import PasswordGenerator from "./components/PasswordGenerator";
 import CurrencyConvertor from "./components/CurrencyConvertor";
 
 function App() {
   return (
     <>
       <div className="wrapper p-5">
+        <RandomBgColor />
+        <PasswordGenerator />
         <CurrencyConvertor />
       </div>
     </>
